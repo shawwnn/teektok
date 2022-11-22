cb522197c5918025898c2704f2e84bafb525de2d
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-22 11:01:21 +0800

    Setup 3: Checking if page loading is SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if(isSSR) return null;
+  
   return <Component {...pageProps} />
 }
