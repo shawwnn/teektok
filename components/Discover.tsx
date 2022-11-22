177d44d01384ba7aaf20049520da76cdeb0a5afe
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-23 07:28:53 +0800

    Footer: initial Footer dynamic Footer footer styling in first part

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -5,8 +5,10 @@ import { topics } from '../utils/constants'
 
 const Discover = () => {
   const router = useRouter()
-  const { topic } = router.query;
-  console.log(topic)
+  const { topic } = router.query
+
+  // const topic = "gaming";
+  // console.log(topic)
 
   const activeTopicStyle = "xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]"
 
