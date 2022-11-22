8a6ecca666675ab7394477f8020361da622d79d9
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-22 23:37:38 +0800

    Discover III: activeTopic style logic but why it is reloads

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -4,7 +4,11 @@ import { useRouter } from 'next/router'
 import { topics } from '../utils/constants'
 
 const Discover = () => {
-  const activeTopicStyle = ""
+  const router = useRouter()
+  const { topic } = router.query;
+  console.log(topic)
+
+  const activeTopicStyle = "xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]"
 
   const topicStyle = "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black"
 
@@ -16,7 +20,7 @@ const Discover = () => {
       <div className="flex gap-3 flex-wrap">
         {topics.map((item) => (
           <Link href={`/topic=${item.name}`} key={item.name} >
-            <div className={topicStyle}>
+            <div className={topic === item.name ? activeTopicStyle : topicStyle}>
               <span className="font-bold text-2xl xl:text-md">
                 {item.icon}
               </span>
