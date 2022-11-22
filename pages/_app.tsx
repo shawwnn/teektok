f94a1a00061f25c9b5291ebfb4bd21ec894e16d8
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-22 12:41:09 +0800

    Setup 4: Install additional dependencies and called the logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [isSSR, setIsSSR] = useState(true);
@@ -11,5 +13,17 @@ export default function App({ Component, pageProps }: AppProps) {
 
   if(isSSR) return null;
   
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar />
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          <Sidebar />
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  )
 }
