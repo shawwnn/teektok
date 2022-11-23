da3c212170beb0770768adfdb899a007a8b23978
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-23 08:12:24 +0800

    Backend II: construct a post query

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { client } from '../../../utils/client';
+import { allPostsQuery } from '../../../utils/queries'
 
 type Data = {
   name: string
@@ -7,7 +9,12 @@ type Data = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'Response Success' })
+	// res.status(200).json({ name: 'Response Success' })
+	if(req.method === 'GET') {
+		const query = allPostsQuery();
+
+		// const data = await client...
+	}
 }
