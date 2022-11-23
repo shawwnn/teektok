a5afcd0dd3b167feccd35eb58e0763c559748ef2
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-23 12:50:21 +0800

    Video View 1: modify next config to accept new domain as image

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Video } from '../types'
 import { NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs' 
+import { GoVerified } from 'react-icons/go'
+
 
 interface IProps {
   post: Video;
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
-	// console.log(post.something); //fails due to not does not exist on type 'Video'
-  console.log(post.caption);
-	return (
-    <div>VideoCard</div>
+  return (
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+      <div className="test">
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold">
+          <div className="md:w-16 md:h-16 w-10 h-10">
+            <Link href="/">
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  className="rounded-full"
+                  src={post.postedBy.image}
+                  alt="profile-photo"
+                  layout="responsive"
+                />
+              </>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
