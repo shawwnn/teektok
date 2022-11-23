ebc4fe51404c86d86c82fbe0aed737084845c170
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-24 00:17:54 +0800

    Google OAuth II: (important) Call the component sign in user is not logged in

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,8 +9,11 @@ import { AiOutlineLogout } from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 import Logo from '../utils/tiktik-logo.png'
+import { GoogleLogin } from '@react-oauth/google'
 
 const Navbar = () => {
+  const user = false;
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -23,6 +26,21 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>
+        Search
+      </div>
+
+      <div>
+        {user ? (
+          <div>Logged in</div>
+        ) : (
+          <GoogleLogin 
+            onSuccess={(response) => console.log(response)} 
+            onError={() => console.log("Error")}
+          />
+        )}
+      </div>
     </div>    
   )
 }
