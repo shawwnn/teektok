54bb222a20844c50e5ffc6276c0bcface255179c
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-25 00:37:25 +0800

    Upload Video VI: Caption part - Html CSS

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -8,6 +8,8 @@ import { SanityAssetDocument } from '@sanity/client'
 import useAuthStore from '../store/authStore'
 import { client } from '../utils/client'
 
+import { topics } from '../utils/constants'
+
 const Upload = () => {
 	const [isLoading, setIsLoading] = useState(false)
 	const [videoAsset, setVideoAsset] = useState<SanityAssetDocument | undefined>()
@@ -37,7 +39,7 @@ const Upload = () => {
 
   return (
 		<div className="flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center">
-			<div className="bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6">
+			<div className="bg-white rounded-lg xl:h-[80vh] w-[60%] flex gap-6 flex-wrap justify-between items-center p-14 pt-6">
 				<div>
 					<div>
 						<p className="text-2xl font-bold">Upload Video</p>
@@ -104,7 +106,44 @@ const Upload = () => {
 
 				<div className="flex flex-col gap-3 pb-10">
 					<label className="text-md font-medium">Caption</label>
-
+					<input 
+						type="text" 
+						value=""
+						onChange={() => {}}
+						className="rounded outline-none text-md border-2 border-gray-200 p-2"
+					/>
+					<label className="text-md font-medium">Choose a category</label>
+					<select 
+						name="test" id="test"
+						onChange={() => {}}
+						className="outline-none border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer"
+					>
+						{topics.map((topic) => (
+							<option
+								key={topic.name}
+								// className="outline-none capitalize bg-white text-gray-700 text-md p-2 hover:bg-slate-300"
+								value={topic.name}
+							>
+								{topic.name}
+							</option>
+						))}
+					</select>
+					<div className="flex gap-6 mt-10">
+						<button
+							onClick={() => {}}
+							type="button"
+							className="border-gray-300 border-2 text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+						>
+							Discard
+						</button>
+						<button
+							onClick={() => {}}
+							type="button"
+							className="bg-[#F51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+						>
+							Post
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
