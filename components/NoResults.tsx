d64245afdca72a717416fe48381477e9a8baf982
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-26 07:52:35 +0800

    Comments I: UI Structure

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { NextPage } from 'next' // needed for complex types
+import { MdOutlineVideocamOff } from 'react-icons/md'
+import { BiCommentX } from 'react-icons/bi'
+
 
 interface IProps {
     text: string;
@@ -9,7 +12,17 @@ interface IProps {
 const NoResults = ({ text }: IProps) => { //the basic
 
   return (
-    <div>NoResults</div>
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className="text-8xl">
+        {/* <MdOutlineVideocamOff /> */}
+        {text.includes('No Comments') 
+          ? <BiCommentX /> 
+          : <MdOutlineVideocamOff />}
+      </p>
+      <p className="text-2xl text-center">
+        {text}
+      </p>
+    </div>
   )
 }
 
