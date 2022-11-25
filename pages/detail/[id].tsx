58a9558ed1915a02c7eec7c6dd1e7b726e3b35b2
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-25 22:11:31 +0800

    Video Details V: Show Captions

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -9,6 +9,8 @@ import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
 import axios from 'axios'
 import { BASE_URL } from '../../utils'
 import { Video } from '../../types'
+import useAuthStore from '../../store/authStore'
+
 
 interface IProps {
 	postDetails: Video,
@@ -20,6 +22,7 @@ const Detail = ({ postDetails } : IProps) => {
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [isVideoMuted, setIsVideoMuted] = useState(false);
 	const router = useRouter();
+	// const { userProfile } = useAuthStore();
 
 	const onVideoClick = () => {
 		if(isPlaying) {
@@ -79,6 +82,44 @@ const Detail = ({ postDetails } : IProps) => {
 							<HiVolumeUp className="text-white text-2xl lg:text-4xl" />
 						</button>
 					)}
+				</div>
+			</div>
+
+			<div className="relative w-[1000px] md:w-[900px] lg:w-[700px]">
+				<div className="lg:mt-20 mt-10">
+					
+					<div className="flex gap-3 p-2 cursor-pointer font-semibold">
+						<div className="md:w-20 md:h-20 w-16 h-16 ml-4">
+							<Link href="/">
+								<>
+									<Image
+										width={62}
+										height={62}
+										className="rounded-full"
+										src={post.postedBy.image}
+										alt="profile-photo"
+										layout="responsive"
+									/>
+								</>
+							</Link>
+						</div>
+
+						<div>
+							<Link href="/">
+								<div className="mt-3 flex flex-col gap-2">
+									<p className="flex gap-2 items-center md:text-md font-bold text-primary">
+										{post.postedBy.userName} {` `}
+										<GoVerified className="text-blue-400 text-md" />
+									</p>
+									<p className="capitalize font-medium text-xs text-gray-500 hidden md:block">{post.postedBy.userName}</p>
+								</div>
+							</Link>
+						</div>  
+					</div>
+					<p className='px-10 text-lg text-gray-600'>{post.caption}</p>
+					<div className="mt-10 px-10">
+						{}
+					</div>
 				</div>
 			</div>
 		</div>
