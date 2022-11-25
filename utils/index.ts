056309cc378340dcb0a05f97d1eb7acce4155048
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-25 15:31:31 +0800

    added constant BASE_URL for global get serverside props for videodetails page

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import jwtDecode from 'jwt-decode';
 // import jwt_decode from 'jwt-decode';
 
 
-// export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const createOrGetUser = async (response: any, addUser: any) => {
   // console.log(response.credential);
@@ -21,7 +21,7 @@ export const createOrGetUser = async (response: any, addUser: any) => {
 
   addUser(user);
 
-  await axios.post(`http://localhost:3000/api/auth`, user);
+  await axios.post(`${BASE_URL}/api/auth`, user);
   
   // var base64Url = response.credential.split('.')[1];
   // var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
