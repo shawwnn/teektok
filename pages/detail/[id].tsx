9c671f2d79f5bb8d9e562481fff67006369db1ad
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-25 21:40:32 +0800

    Video Details IV: Back Button functionality

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -19,6 +19,7 @@ const Detail = ({ postDetails } : IProps) => {
 	const videoRef = useRef<HTMLVideoElement>(null);
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [isVideoMuted, setIsVideoMuted] = useState(false);
+	const router = useRouter();
 
 	const onVideoClick = () => {
 		if(isPlaying) {
@@ -43,7 +44,7 @@ const Detail = ({ postDetails } : IProps) => {
     <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
 			<div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black">
 				<div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
-					<p>
+					<p className="cursor-pointer" onClick={() => router.back()}>
 						<MdOutlineCancel className="text-white text-[35px]" />
 					</p>
 				</div> 
