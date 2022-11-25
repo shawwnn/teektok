b16bbb818e58484a07234e35f5813c599c745b38
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-25 14:07:51 +0800

    Upload Video A(clean-up): Fix CSS of video exceeding the border

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -72,7 +72,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
             <video
               ref={videoRef}
               loop
-              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100 outline-doubl"
               src={post.video.asset.url}
             >
 
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -84,7 +84,7 @@ const Upload = () => {
 						<p className="text-2xl font-bold">Upload Video</p>
 						<p className="text-md text-gray-400 mt-1">Post a video to your account</p>
 					</div>
-					<div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100">
+					<div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-5/6 p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100">
 						{isLoading ? (
 							<p>Uploading...</p>
 						) : (
@@ -95,7 +95,7 @@ const Upload = () => {
 											src={videoAsset.url}
 											loop
 											controls
-											className="rounded-xl h-[450px] mt-16 bg-black"
+											className="rounded-xl h-[450px] mt-0 bg-black"
 										>
 
 										</video>
@@ -103,7 +103,7 @@ const Upload = () => {
 								) : (
 									<label 
 										// htmlFor="" 
-										className="cursor-pointer">
+										className="cursor-pointer h-[80vh]">
 										<div className="flex flex-col items-center justify-center h-full">
 											<div className="flex flex-col items-center justify-center">
 												<p className="font-bold text-xl">
