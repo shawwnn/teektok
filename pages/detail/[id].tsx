ea402b7440c9c0fe641c5a18604a641716e114cf
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-26 12:14:55 +0800

    Comments II: handle add Comment declaration and typescripts passing of props

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -26,6 +26,8 @@ const Detail = ({ postDetails } : IProps) => {
 	const [isVideoMuted, setIsVideoMuted] = useState(false);
 	const router = useRouter();
 	const { userProfile }: any = useAuthStore();
+	const [comment, setComment] = useState('');
+	const [isPostingComment, setIsPostingComment] = useState(false);
 
 	const onVideoClick = () => {
 		if(isPlaying) {
@@ -53,6 +55,19 @@ const Detail = ({ postDetails } : IProps) => {
 			})
 
 			setPost({ ...post, likes: data.likes });
+		}
+	}
+
+	const addComment = async (e) => {
+		e.preventDefault();
+
+		if(userProfile && comment) {
+			setIsPostingComment(true);
+
+			const response = await axios.put(`${BASE_URL}/api/post/${post._id}`,{
+				userId: userProfile._id,
+				comment
+			});
 		}
 	}
 
@@ -142,7 +157,12 @@ const Detail = ({ postDetails } : IProps) => {
 						)}
 					</div>
 					<Comments
-					
+						comment={comment}
+						setComment={setComment}
+						addComment={addComment}
+						comments={post.comments}
+						isPostingComment={isPostingComment}
+
 					/>
 				</div>
 			</div>
