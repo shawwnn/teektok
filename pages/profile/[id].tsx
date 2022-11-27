3f2b4a70726f654afecd68294c16da72901f6cfd
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-27 23:48:13 +0800

    Profile 4: deconstruct the fetch data copy in the suggested accounts the code for image and username UI

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -19,8 +19,32 @@ interface IProps {
 
 
 const Profile = ({ data }: IProps) => {
-  return (
-		<div>Profile</div>
+  const { user, userVideos, userLikedVideos } = data;
+	
+	return (
+		<div className="w-full">
+			<div className="flex gap-6 md:gap-10 mb-4 bg-white w-full">
+				<div className="w-16 h-16 md:w-32 md:h-32 ">
+					<Image 
+						src={user.image}
+						width={120}
+						height={120}
+						className="rounded-full"
+						alt="user-profile"
+						layout="responsive"
+
+					/>
+				</div>
+
+				<div className="flex flex-col justify-center">
+					<p className="md:text-2xl tracking-wider flex gap-1 items-center justify-center text-md font-bold text-primary lowercase">
+						{user.userName.replaceAll(' ', '')}
+						<GoVerified className="text-blue-400"/>
+					</p>
+					<p className="capitalize md:text-xl text-gray-400 text-xs">{user.userName}</p>
+				</div>
+			</div>
+		</div>
 	)
 }
 
