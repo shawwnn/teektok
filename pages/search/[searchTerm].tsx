11bbc5a682030a84f17cdc19eb40da06ec21b81a
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-28 17:39:10 +0800

    Search IV: (Important) show Accounts in when they are searched get All users through authstore filter function it and includes function it copy the code for showing UI names and usernames in the comments component

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -15,10 +15,14 @@ import useAuthStore from "../../store/authStore";
 const Search = ({ videos }: { videos: Video[] }) => {
 	const [isAccounts, setIsAccounts] = useState(false);
 	const router = useRouter();
-	const { searchTerm } = router.query;
+	const { searchTerm }:any = router.query;
+
+	const { allUsers } = useAuthStore();
 
 	const hasAccounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
 	const hasVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+
+	const searchedAccounts = allUsers.filter((user: IUser) => user.userName.toLowerCase().includes(searchTerm.toLowerCase()));
  
 	return (
     <div className="w-full">
@@ -27,7 +31,39 @@ const Search = ({ videos }: { videos: Video[] }) => {
 				<p className={`text-xl font-semibold cursor-pointer mt-2 ${hasVideos}`} onClick={() => setIsAccounts(false)}>Videos</p>
 			</div>
 			{isAccounts ? (
-				<div>Accounts</div>
+				<div className="md:mt-16">
+					{searchedAccounts.length > 0 ? (
+						searchedAccounts.map((user: IUser, idx: number) => (
+							<Link href={`/profile/${user._id}`} key={idx}>
+								<div className="flex items-start gap-3">
+									<div>
+										<Image 
+											src={user.image}
+											width={50}
+											height={50}
+											className="rounded-full"
+											alt="user-profile"
+											//layout="responsive"
+
+										/>
+									</div>
+
+									<div className="hidden xl:block">
+										<p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+											{user.userName.replaceAll(' ', '')}
+											<GoVerified className="text-blue-400"/>
+										</p>
+										<p className="capitalize text-gray-400 text-xs">{user.userName}</p>
+									</div>
+								</div>
+							</Link>
+						))
+					) : 
+						<NoResults 
+							text={`No video results for ${searchTerm}`} 
+						/>
+					}
+				</div>
 			) : (
 				<div className="md:mt-16 flex flex-wrap gap-6 md:justify-start">
 					{videos.length ? (
