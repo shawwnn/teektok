07146dc81b075585f02f9fd9678cb2325812dbb6
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-29 18:47:16 +0800

    edit css

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -35,7 +35,7 @@ const Search = ({ videos }: { videos: Video[] }) => {
 					{searchedAccounts.length > 0 ? (
 						searchedAccounts.map((user: IUser, idx: number) => (
 							<Link href={`/profile/${user._id}`} key={idx}>
-								<div className="flex items-start gap-3">
+								<div className="flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200">
 									<div>
 										<Image 
 											src={user.image}
